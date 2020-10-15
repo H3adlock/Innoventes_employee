86d09d3166c9757ba8a555c94a6a7ccaf45d1776
Sourav Ghosh <sourav.ghosh700@hotmail.com>
2020-10-16 00:40:13 +0530

    minor bug in serializer fixed

--- employee/serializers.py
+++ employee/serializers.py
@@ -29,24 +29,24 @@ class EmployeeSerializer(serializers.ModelSerializer):
         return employee
 
     def update(self, instance, validated_data):
-        addresses_data = validated_data.pop('address_set')
-        addresses = (instance.address_set).all()
-        addresses = list(addresses)
         instance.first_name = validated_data.get('first_name', instance.first_name)
         instance.last_name = validated_data.get('last_name', instance.last_name)
         instance.birth_date = validated_data.get('birth_date', instance.birth_date)
         instance.designation = validated_data.get('designation', instance.designation)
         instance.save()
-
-        for single_address in addresses_data:
-            address = addresses.pop(0)
-            address.address_type = single_address.get('address_type', address.address_type)
-            address.address_line_1 = single_address.get('address_line_1', address.address_line_1)
-            address.address_line_2 = single_address.get('address_line_2', address.address_line_2)
-            address.city = single_address.get('city', address.city)
-            address.pin = single_address.get('pin', address.pin)
-            address.country = single_address.get('release_date', address.country)
-            address.save()
+        if 'address_set' in validated_data:
+            addresses_data = validated_data.pop('address_set')
+            addresses = (instance.address_set).all()
+            addresses = list(addresses)
+            for single_address in addresses_data:
+                address = addresses.pop(0)
+                address.address_type = single_address.get('address_type', address.address_type)
+                address.address_line_1 = single_address.get('address_line_1', address.address_line_1)
+                address.address_line_2 = single_address.get('address_line_2', address.address_line_2)
+                address.city = single_address.get('city', address.city)
+                address.pin = single_address.get('pin', address.pin)
+                address.country = single_address.get('release_date', address.country)
+                address.save()
         return instance
 
 
@@ -59,7 +59,7 @@ class EmployeeOnlySerializer(serializers.ModelSerializer):
 class AddressByEmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
-        fields = ('id','employee', 'address_type',
+        fields = ('id', 'employee', 'address_type',
                   'address_line_1',
                   'address_line_2',
                   'city',
